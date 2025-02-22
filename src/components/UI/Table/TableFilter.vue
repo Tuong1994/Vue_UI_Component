3342fa1cd398584a1735c8c5d4795b7104c71bdc
Tuong1994 <nhambontuong68@gmail.com>
2025-02-22 22:01:04 +0700

    Fix Table

--- src/components/UI/Table/TableFilter.vue
+++ src/components/UI/Table/TableFilter.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { UI } from '@/components'
 import type { TableColor } from './type'
+import useLangStore from '@/stores/LangStore';
 
 const { Button, Space, Flex } = UI
 
@@ -14,18 +15,24 @@ defineProps<TableFilterProps>()
 
 const emits = defineEmits(['onFilter', 'onCancelFilter'])
 
+const t = useLangStore()
+
 const handleFilter = () => emits('onFilter')
 
 const handleCancelFilter = () => emits('onCancelFilter')
 </script>
 
 <template>
-  <FlexRow rootClassName="table-fitler">
+  <FlexRow rootClassName="table-filter">
     <slot name="filter"></slot>
     <FlexCol>
       <Space>
-        <Button sizes="sm" :color="color" @click="handleFilter"> Save </Button>
-        <Button sizes="sm" ghost @click="handleCancelFilter"> Cancel </Button>
+        <Button sizes="sm" :color="color" @click="handleFilter">
+          {{ t.lang.common.actions.save }}
+        </Button>
+        <Button sizes="sm" ghost @click="handleCancelFilter">
+          {{ t.lang.common.actions.cancel }}
+        </Button>
       </Space>
     </FlexCol>
   </FlexRow>
